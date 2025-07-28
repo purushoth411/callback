61c22bf0c86418e53e666a9cc8f9f92daa43b55b
saro-dev <codersaro@gmail.com>
2025-07-28 11:02:06 +0530

    new

--- models/dashboardModel.js
+++ models/dashboardModel.js
@@ -1,16 +1,18 @@
 // models/dashboardModel.js
 const db = require("../config/db"); // Update path if needed
+const moment = require('moment');
+
 
 const getAllActiveTeams = (callback) => {
-    const sql = `
+  const sql = `
     SELECT * FROM tbl_team
     WHERE status = 'Active'
     ORDER BY fld_addedon DESC
   `;
-    db.query(sql, (err, results) => {
-        if (err) return callback(err, null);
-        return callback(null, results);
-    });
+  db.query(sql, (err, results) => {
+    if (err) return callback(err, null);
+    return callback(null, results);
+  });
 };
 
 const getTotalData = async ({
@@ -70,6 +72,39 @@ const getTotalData = async ({
     conditions.push(`LOWER(TRIM(fld_converted_sts)) = 'yes'`);
   }
 
+   if (filter_type) {
+    let startDate = null;
+    let endDate = null;
+
+    switch (filter_type) {
+      case 'Today':
+        startDate = moment().startOf('day').format('YYYY-MM-DD HH:mm:ss');
+        endDate = moment().endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        break;
+
+      case 'Week':
+        startDate = moment().startOf('week').format('YYYY-MM-DD HH:mm:ss');
+        endDate = moment().endOf('week').format('YYYY-MM-DD HH:mm:ss');
+        break;
+
+      case 'Month':
+        startDate = moment().startOf('month').format('YYYY-MM-DD HH:mm:ss');
+        endDate = moment().endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        break;
+
+      case 'Last':
+        startDate = moment().subtract(1, 'months').startOf('month').format('YYYY-MM-DD HH:mm:ss');
+        endDate = moment().subtract(1, 'months').endOf('month').format('YYYY-MM-DD HH:mm:ss');
+        break;
+    }
+
+    if (startDate && endDate) {
+      conditions.push(`fld_addedon BETWEEN ? AND ?`);
+      params.push(startDate, endDate);
+    }
+  }
+
+
   if (conditions.length > 0) {
     query += ` WHERE ` + conditions.join(' AND ');
   }
@@ -98,13 +133,42 @@ const getTotalData = async ({
   });
 };
 
+const getParticularStatusCallsOfCrm = (crm_id, status, callback) => {
+  const currentDate = moment();
+  const twoDaysBefore = currentDate.clone().subtract(2, 'days').format('YYYY-MM-DD');
+  const twoDaysAfter = currentDate.clone().add(2, 'days').format('YYYY-MM-DD');
 
+  let sql = `
+    SELECT * FROM tbl_booking
+    WHERE fld_call_request_sts = ?
+      AND fld_consultation_sts = ?
+      AND fld_booking_date BETWEEN ? AND ?
+      AND callDisabled IS NULL
+  `;
+
+  const params = [status, status, twoDaysBefore, twoDaysAfter];
+
+  if (crm_id) {
+    sql += ` AND fld_addedby = ?`;
+    params.push(crm_id);
+  }
+
+  db.query(sql, params, (err, results) => {
+    if (err) return callback(err, null);
+    return callback(null, results);
+  });
+};
+
+module.exports = {
+  getParticularStatusCallsOfCrm
+};
 
 
 
 
 
 module.exports = {
-    getAllActiveTeams,
-    getTotalData
+  getAllActiveTeams,
+  getTotalData,
+  getParticularStatusCallsOfCrm
 }
