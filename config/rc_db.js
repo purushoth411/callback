19c435313de440dc1a552883f859e666871dce2b
purushoth411 <rkip4112001@gmail.com>
2025-08-14 10:28:54 +0530

    mysql2

--- config/rc_db.js
+++ config/rc_db.js
@@ -1,25 +1,28 @@
-var mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-var connection = mysql.createPool({
-    connectionLimit: 5, // lower number for HostGator
-    host: '50.87.148.156',
-    user: 'rapidcol_rc_main',
-    password: 'rc_main#123',
-    database: 'rapidcol_rc_main',
-    charset: 'utf8mb4',
-    connectTimeout: 30000,
-    timezone: 'local', // avoid forcing 'Asia/Kolkata' here
-    ssl: false // explicitly disable SSL unless required
+const pool = mysql.createPool({
+  host: '50.87.148.156',
+  user: 'rapidcol_rc_main',
+  password: 'rc_main#123',
+  database: 'rapidcol_rc_main',
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 60000, // 60 seconds
+  charset: 'utf8mb4',
+  ssl: false // if your DB doesn’t require SSL
 });
 
-// Test connection
-connection.getConnection((err, conn) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        process.exit(1);
-    }
+async function testConnection() {
+  try {
+    const conn = await pool.getConnection();
     console.log('Connected to Rc database');
     conn.release();
-});
+  } catch (err) {
+    console.error('Error connecting to the database:', err);
+  }
+}
 
-module.exports = connection;
+testConnection();
+
+module.exports = pool;
